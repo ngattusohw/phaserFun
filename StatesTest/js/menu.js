5065590703d1a570e2224c0e886ab9df24526de6
ngattusohw <ngattusohw@gmail.com>
2017-03-27 19:42:46 -0400

    Adding first working state test game

--- StatesTest/js/menu.js
+++ StatesTest/js/menu.js
@@ -1,10 +1,20 @@
 var menuState = {
 
-	create: funciton(){
+	create: function(){
 
 		var nameLabel = game.add.text(80, 150, 'First test game',
 			{font: '50px Arial', fill: '#ffffff' });
 
-		
+		var startLabel = game.add.text(80, game.world.height-80,
+			'press the W key to start',
+			{font: '25px Arial', fill: '#ffffff'});
+
+		var wkey = game.input.keyboard.addKey(Phaser.Keyboard.W);
+
+		wkey.onDown.addOnce(this.start, this);
+	},
+
+	start: function(){
+		game.state.start('play');
 	}
 }
